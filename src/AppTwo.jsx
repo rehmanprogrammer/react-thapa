a31d3c6c0160e6666e44130145fd462e8c3dd9f4
rehmanprogrammer <rehmanprogrammer105@gmail.com>
2024-12-29 13:34:01 +0500

    Change files

---
+++ src/AppTwo.jsx
@@ -0,0 +1,3 @@
+export const Thapa = () => {
+    return <h1>Hello World</h1>
+}
--- src/mainTwo.jsx
+++ src/mainTwo.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import "./indexTwo.css";
+import {Thapa} from "./AppTwo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Thapa />
   </StrictMode>
 );
